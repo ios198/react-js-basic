16ba4841ab855522e4fe5b559c4e1bc74e94f170
dhquan <danghongquan198@gmail.com>
2024-01-02 06:25:54 +0700

    Dome Events

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 
 const TodoList = () => {
   const [name, setName] = useState();
+  const handleClickBt = (event, mgs) => {
+    console.log(">>run handleClickBt function", mgs);
+  };
   return (
     <div>
       <label>Name: </label>
       <input
         value={name}
         type="text"
-        onChange={(event) => setName(event.target.value)}
+        onChange={(event) => {
+          //console.log("check event", event);
+          setName(event.target.value);
+        }}
       />
+      <button
+        type="submit"
+        onClick={(event) => {
+          console.log("check event", event);
+          handleClickBt(event, "buttonClick");
+        }}
+      >
+        Submit
+      </button>
       <br />
       <br />
       Hello Todo List with the name: {name}
