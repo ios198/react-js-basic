8c0a9cba0c2769616551206845e33423a69d907b
dhquan <danghongquan198@gmail.com>
2024-01-09 06:01:39 +0700

    Link & NavLink in Router

--- src/App.js
+++ src/App.js
@@ -4,25 +4,44 @@ import Home from "./components/Home";
 import AddNewProduct from "./components/AddNewProduct";
 import Product from "./components/Products/Product";
 import "react-image-lightbox/style.css";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 const App = () => {
   const x = "Quan";
   return (
-    <div className="App">
-      <header className="App-header content-left">
-        <div style={{ textAlign: "center" }}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p>
-          <span>Xin chao {x}</span>
-        </p>
-        <Home />
-      </header>
-      <div className="content-right">
-        <AddNewProduct />
-        <hr />
-        <Product />
-      </div>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/product">
+          <Product />
+        </Route>
+        <Route path="/weather">
+          <div>Weather App</div>
+        </Route>
+        <Route path="/about">
+          <div>About</div>
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <header className="App-header content-left">
+              <div style={{ textAlign: "center" }}>
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+              <p>
+                <span>Xin chao {x}</span>
+              </p>
+              <Home />
+            </header>
+            <div className="content-right">
+              <AddNewProduct />
+              <hr />
+              <Product />
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,17 @@
+import "./Nav.scss";
+import { Link, NavLink } from "react-router-dom";
+const Nav = () => {
+  return (
+    <div>
+      <div class="topnav">
+        <NavLink class="active" exact={true} to="/">
+          Home
+        </NavLink>
+        <NavLink to="/product">Product</NavLink>
+        <NavLink to="/weather">Weather App</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </div>
+    </div>
+  );
+};
+export default Nav;
